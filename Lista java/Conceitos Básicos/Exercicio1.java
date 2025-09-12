bc795495907eb09e03a9a2e82a5bab7524c12618
Ives de Arruda Xavier Botelho <ives.botelho@liberali.local>
2025-09-12 09:29:07 -0400

    Feat: Resolução do Exercício 1

---
+++ Lista java/Conceitos Básicos/Exercicio1.java
@@ -0,0 +1,8 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+        String mensagem_1 = """
+                Olá, Mundo!""";
+        System.out.println(mensagem_1);
+    }
+    
+}
