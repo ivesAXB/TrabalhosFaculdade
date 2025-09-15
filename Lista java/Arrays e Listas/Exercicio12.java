75e48665ee4b7a1c3b4048b2688839136404dabf
Ives de Arruda Xavier Botelho <ives.botelho@liberali.local>
2025-09-15 16:20:19 -0400

    Stop: fazer um for para atribuir a primeira e a segunda metade da array para ordenar aos respectivos arrays

---
+++ Lista java/Arrays e Listas/Exercicio12.java
@@ -0,0 +1,29 @@
+/*import java.util.Scanner;
+
+public class Exercicio12 {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Quantos números irá digitar? ");
+        int n = leitor.nextInt();
+        System.out.println("Digite uma sequencia de números");
+        int[] inteiros = new int[n];
+
+        int maior_numero = 0;
+
+        for (int i = 0; i < n; i++){
+            inteiros[i] = leitor.nextInt();   
+        }
+
+        leitor.close();
+    }
+    public static int[] crescente(int[] arrayParaOrdenar){
+            if (arrayParaOrdenar.length <= 1){
+                return arrayParaOrdenar;
+            } else {
+                int[] metadeesquerda = arrayParaOrdenar[]
+                int[] metadeEsquerdaOrdenada = crescente(metadeesquerda)
+                int[] metadeDireitaOrdenada = crescente(metadedireita)
+            }
+        }
+}*/
