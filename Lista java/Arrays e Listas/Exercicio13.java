34fd2d73227680be1b29dd11b52b63f97035b25f
ivesAXB <138529884+ivesAXB@users.noreply.github.com>
2025-09-17 20:20:44 -0400

    Update Exercicio13.java
    Update manual

---
+++ Lista java/Arrays e Listas/Exercicio13.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class exercicio13 {
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Quantos valores vai calcular? ");
+        int n = leitor.nextInt();
+
+        System.out.println("Digite os valores. ");
+        int[] valores_para_media = new int[n];
+
+        int valor_total = 0;
+
+        for(int i = 0; i < n; i++){
+            valores_para_media[i] = leitor.nextInt();
+        }
+        for(int i = 0; i < n; i++){
+            valor_total = valor_total + valores_para_media[i];
+        }
+
+        double calc_media = valor_total/n;
+
+        System.out.printf("A média da lista de valores é: %.2f", calc_media);
+
+        leitor.close();
+    }
+}
